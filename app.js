cd5b534ecd72ad8f79e620baab52bf391a1b4391
Franco C <emmanuelcardosoahz@gmail.com>
2025-03-18 06:16:55 -0500

    Ingreso de nombres y visualizacion de los mismos

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = []
+//Inicializamos la lista donde guardaremos los nombres de los amigos
+let amigos = [];
 
-console.log(``)
+//Elegimos los archivos del html
+let nombreAmigo = document.getElementById("amigo");
+
+//Creamos una variable para tener el lugar donde se pondra la lista de amigos
+let visualAmigos = document.getElementById("listaAmigos");
+
+
+//Hacemos una funcion para agregar a los amigos una vez
+//Que se apriete el boton
+function agregarAmigo(){
+    amigos.push(nombreAmigo.value);
+
+    //Agregamos los nombres de las personas al ser añadidas
+    visualAmigos.innerHTML += `<l1> ${nombreAmigo.value} </l1>`;
+};
