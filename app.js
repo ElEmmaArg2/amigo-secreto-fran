0d273083e285ddc4bcd176a4fcb142456ce85fe7
Franco C <emmanuelcardosoahz@gmail.com>
2025-03-18 07:08:18 -0500

    Realizamos la limpieza de lo estetico, y corregimos posibles errores con repeticion de varios ganadores

--- app.js
+++ app.js
@@ -15,14 +15,17 @@ let ganador = document.getElementById("resultado")
 //Hacemos una funcion para agregar a los amigos una vez
 //Que se apriete el boton
 function agregarAmigo(){
-    amigos.push(nombreAmigo.value);
-
 
 //Controlador de que esta correcto los nombres
 
     if(!nombreAmigo.value){
-        alert("Valor invalido")
-    }
+        alert("Valor invalido... Por favor ingrese un nombre")
+        return;
+    }else{
+        //Si la cadena no esta vacia agrego el nombre
+        amigos.push(nombreAmigo.value);
+
+    };
 
     //Agregamos los nombres de las personas al ser añadidas
     visualAmigos.innerHTML += `<li> ${nombreAmigo.value} </li>`;
@@ -30,13 +33,22 @@ function agregarAmigo(){
 
 //Realizamos el funcionamiento de sortear amigo
 function sortearAmigo(){
+    
+    if(amigos.length === 0){
+        alert("No hay amigos para sortear");
+        return;
+    }
+
     let posAleatoria = Math.floor((Math.random() * amigos.length));
     let amigoSecreto = amigos[posAleatoria];
-
+    let limpiarLista = document.getElementById("listaAmigos");
     //Realizamos la visualizacion del ganador del amigo secreto
 
     ganador.innerHTML = `<li>El ganador del amigo secreto es: ${amigoSecreto}</li>`
 
-
-
+    //Limpiamos los nombres y al tener ya un ganador, tambien los anteriores participantes 
+    limpiarLista.innerHTML = "";
+    amigos= [];
+    return
 };
+
